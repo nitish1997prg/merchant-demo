f42f7c3beb20f6e4d1fe3704cc37c17e2e1d1e60
Nitish Viraktamath <mathnitish1997@gmail.com>
2026-08-06 11:40:28 +0530

    Added OpenTelemetry Tracing

--- services/webhookService.js
+++ services/webhookService.js
@@ -1,21 +1,28 @@
 import {Order} from "../models/Order.js";
+import {withSpan} from "../telemetry/withSpan.js";
 
 export async function findOrder(id){
     return await Order.findOne({orderId: id});
 }
 
 export async function processPaymentWebhook(order,data){
-    try {
+    return withSpan("Process Payment Webhook",async (span)=>{
+        span.setAttribute("payment.paymentId",data.paymentId);
+        try {
         order.status = "paid";
         order.paidAt = new Date();
         order.paymentId = data.paymentId;
 
-        await order.save();
+        await withSpan("Save Order",async ()=>{
+             await order.save();
+        });
 
     }catch(error){
         console.error("Error processing payment webhook!",error);
         throw error;
     }
+    })
+   
 }
 
 export const WebhookService = {
